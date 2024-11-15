3398ac70a03e2e31755b2f0f6d0acf1eb303e547
DerickNorlan <dnorlan01@student.ciputra.ac.id>
2024-11-15 08:47:49 +0800

    p

--- portofolio-derick/resources/views/welcome.blade.php
+++ portofolio-derick/resources/views/welcome.blade.php
@@ -39,16 +39,16 @@
     <link href="https://fonts.googleapis.com/css?family=Kaushan+Script" rel="stylesheet">
 
     <!-- Animate.css -->
-    <link rel="stylesheet" href="{{ asset('assets/css/animate.css') }}">
+    <link rel="stylesheet" href="/assets/css/animate.css">
     <!-- Icomoon Icon Fonts-->
-    <link rel="stylesheet" href="{{ asset('assets/css/icomoon.css') }}">
+    <link rel="stylesheet" href="/assets/css/icomoon.css">
     <!-- Bootstrap  -->
-    <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.css') }}">
+    <link rel="stylesheet" href="/assets/css/bootstrap.css">
     <!-- Theme style  -->
-    <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
+    <link rel="stylesheet" href="/assets/css/style.css">
 
     <!-- Modernizr JS -->
-    <script src="{{ asset('assets/js/modernizr-2.6.2.min.js') }}"></script>
+    <script src="/assets/js/modernizr-2.6.2.min.js"></script>
     <!-- FOR IE9 below -->
     <!--[if lt IE 9]>
 	<script src="js/respond.min.js"></script>
@@ -527,20 +527,20 @@
     </div>
 
     <!-- jQuery -->
-    <script src="{{ asset('assets/js/jquery.min.js') }}"></script>
+    <script src="/assets/js/jquery.min.js"></script>
     <!-- jQuery Easing -->
-    <script src="{{ asset('assets/js/jquery.easing.1.3.js') }}"></script>
+    <script src="/assets/js/jquery.easing.1.3.js"></script>
     <!-- Bootstrap -->
-    <script src="{{ asset('assets/js/bootstrap.min.js') }}"></script>
+    <script src="/assets/js/bootstrap.min.js"></script>
     <!-- Waypoints -->
-    <script src="{{ asset('assets/js/jquery.waypoints.min.js') }}"></script>
+    <script src="/assets/js/jquery.waypoints.min.js"></script>
     <!-- Stellar Parallax -->
-    <script src="{{ asset('assets/js/jquery.stellar.min.js') }}"></script>
+    <script src="/assets/js/jquery.stellar.min.js"></script>
     <!-- Easy PieChart -->
-    <script src="{{ asset('assets/js/jquery.easypiechart.min.js') }}"></script>
+    <script src="/assets/js/jquery.easypiechart.min.js"></script>
 
     <!-- Main -->
-    <script src="{{ asset('assets/js/main.js') }}"></script>
+    <script src="/assets/js/main.js"></script>
     
 
 </body>
